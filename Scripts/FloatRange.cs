72d020fd14a5fd39d43b8e0c1558acc22e825500
Codey Huntting <creativecodey@gmail.com>
2022-01-30 19:23:25 -0800

    Added something to float range
    Also added an absolute value for vectors

--- Scripts/FloatRange.cs
+++ Scripts/FloatRange.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 [System.Serializable]
 public struct FloatRange
 {
+    #region Private Editor Fields
     [SerializeField]
     [Tooltip("Minimum value of the float")]
     private float _min;
     [SerializeField]
     [Tooltip("Maximum value of the float")]
     private float _max;
+    #endregion
 
+    #region Public Properties
     public float min => _min;
     public float max => _max;
     public float length => _max - _min;
+    #endregion
 
+    #region Constructors
     public FloatRange(float min, float max)
     {
         if(min > max)
@@ -28,4 +33,12 @@ public struct FloatRange
         _min = min;
         _max = max;
     }
+    #endregion
+
+    #region Public Methods
+    public float GetInterpolator(float number)
+    {
+        return (number - min) / length;
+    }
+    #endregion
 }
