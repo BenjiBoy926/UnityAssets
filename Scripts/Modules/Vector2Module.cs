45b676746f146ff7eab417c0d7670e231c866a44
Codey Huntting <creativecodey@gmail.com>
2022-01-30 19:55:46 -0800

    Added absolute value for vector2

---
+++ Scripts/Modules/Vector2Module.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector2Module
+{
+    public static Vector2 Abs(this Vector2 vector)
+    {
+        return new Vector2(
+            Mathf.Abs(vector.x),
+            Mathf.Abs(vector.y));
+    }
+}
