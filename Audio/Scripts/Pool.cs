0454310aa748798af9497f2f729b89cb40ef63b7
Codey Huntting <creativecodey@gmail.com>
2022-04-03 15:27:35 -0700

    Added the pool

---
+++ Audio/Scripts/Pool.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T>
+{
+    #region Private Fields
+    private List<T> items = new List<T>();
+    private int currentIndex = 0;
+
+    private Func<T> generator;
+    private Predicate<T> usable;
+    #endregion
+
+    #region Constructors
+    public Pool(int initialItems, Func<T> generator, Predicate<T> usable)
+    {
+        this.generator = generator;
+        this.usable = usable;
+
+        // Use the generator to generat the initial items
+        for (int i = 0; i < initialItems; i++)
+        {
+            items.Add(generator.Invoke());
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public T Get()
+    {
+        T current = items[currentIndex];
+
+        // If the current item is not usable then 
+        // add a new one to the pool of items
+        if (!usable.Invoke(current))
+        {
+            current = generator.Invoke();
+            items.Add(current);
+            currentIndex = items.Count - 1;
+        }
+
+        // Update the index of the current item
+        UpdateCurrentIndex();
+
+        // Return the current item
+        return current;
+    }
+    #endregion
+
+    #region Private Methods
+    private void UpdateCurrentIndex()
+    {
+        int start = currentIndex;
+        currentIndex = (currentIndex + 1) % items.Count;
+
+        // Continue incrementing the index until it hits an item that is usable
+        // or until it loops back around to the start
+        while (currentIndex != start && !usable.Invoke(items[currentIndex]))
+        {
+            currentIndex = (currentIndex + 1) % items.Count;
+        }
+    }
+    #endregion
+}
