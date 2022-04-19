865fd07edbd01b7ce06b22f62a0a52ec762f7fde
Codey Huntting <creativecodey@gmail.com>
2022-04-18 18:16:09 -0700

    Make usable anytime an item is generated

--- Scripts/Pool.cs
+++ Scripts/Pool.cs
@@ -11,29 +11,21 @@ public class Pool<T> where T : class
     private int currentIndex = 0;
 
     private Func<T> generator;
-    private Predicate<T> usable;
+    private Predicate<T> isUsable;
     private Action<T> makeUsable;
     #endregion
 
     #region Constructors
-    public Pool(int initialItems, Func<T> generator, Predicate<T> usable, Action<T> makeUsable)
+    public Pool(int initialItems, Func<T> generator, Predicate<T> isUsable, Action<T> makeUsable)
     {
         this.generator = generator;
-        this.usable = usable;
+        this.isUsable = isUsable;
         this.makeUsable = makeUsable;
 
         // Use the generator to generate the initial items
         for (int i = 0; i < initialItems; i++)
         {
-            T item = generator.Invoke();
-            makeUsable.Invoke(item);
-
-            // Check to make sure the condition of the action is met
-            if (!usable.Invoke(item))
-                Debug.LogWarning($"Item '{item}' is not usable " +
-                    $"after invoking 'makeUsable' on it");
-
-            items.Add(item);
+            GenerateItem();
         }
 
     }
@@ -45,15 +37,14 @@ public class Pool<T> where T : class
         T current = items[currentIndex];
 
         // If this object is null we need to generate a new one
-        if (ReferenceEquals(current, null))
+        if (current is null)
             current = generator.Invoke();
 
         // If the current item is not usable then 
         // add a new one to the pool of items
-        if (!usable.Invoke(current))
+        if (!isUsable.Invoke(current))
         {
-            current = generator.Invoke();
-            items.Add(current);
+            current = GenerateItem();
             currentIndex = items.Count - 1;
         }
 
@@ -66,6 +57,19 @@ public class Pool<T> where T : class
     #endregion
 
     #region Private Methods
+    private T GenerateItem()
+    {
+        T item = generator.Invoke();
+        makeUsable.Invoke(item);
+        items.Add(item);
+
+        // Check to make sure the condition of the action is met
+        if (!isUsable.Invoke(item))
+            Debug.LogWarning($"Item '{item}' is not usable " +
+                $"after invoking 'makeUsable' on it");
+
+        return item;
+    }
     private void UpdateCurrentIndex()
     {
         int start = currentIndex;
@@ -73,7 +77,7 @@ public class Pool<T> where T : class
 
         // Continue incrementing the index until it hits an item that is usable
         // or until it loops back around to the start
-        while (currentIndex != start && !usable.Invoke(items[currentIndex]))
+        while (currentIndex != start && !isUsable.Invoke(items[currentIndex]))
         {
             currentIndex = (currentIndex + 1) % items.Count;
         }
