f330da14194a330f6eea8115f9aec1d49c3fc795
Codey Huntting <creativecodey@gmail.com>
2022-04-29 21:15:22 -0700

    Added redacted substring utility

--- Scripts/Modules/StringModule.cs
+++ Scripts/Modules/StringModule.cs
@@ -13,4 +13,23 @@ public static class StringModule
     {
         return str.Split(' ').Length * wordsPerSecond;
     }
+    /// <summary>
+    /// Get substring. If there is text before or after the substring in the original string,
+    /// appends ellipsis '...' on the end
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="index"></param>
+    /// <param name="length"></param>
+    /// <returns></returns>
+    public static string RedactedSubstring(this string str, int index, int length)
+    {
+        string result = "";
+        index = Mathf.Max(index, 0);
+        if (index > 0) result += "...";
+        if (index + length >= str.Length)
+            result += str.Substring(index);
+        else
+            result += $"{str.Substring(index, length)}...";
+        return result;
+    }
 }
