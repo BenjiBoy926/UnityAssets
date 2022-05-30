56e738a7057056492d5a8b8f59acb4c6fac4cbe0
Codey Huntting <creativecodey@gmail.com>
2022-05-29 20:11:32 -0700

    Ehanced property drawer working well
    All that is left now is to write reams of code that delegate to all of the existing editor gui methods

--- Scripts/Editor/EnhancedPropertyDrawer.cs
+++ Scripts/Editor/EnhancedPropertyDrawer.cs
@@ -4,107 +4,345 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class EnhancedPropertyDrawer : PropertyDrawer
+public abstract class EnhancedPropertyDrawer : PropertyDrawer
 {
     #region Fields
-    protected bool _editorGUI = false;
-    protected float _propertyHeight = 0f;
+    private Rect _currentPosition = Rect.zero;
+    private object _currentControlResult = null;
+    #endregion
+
+    #region Properties
+    protected bool EditorGUI { get; private set; } = false;
+    protected float PropertyHeight { get; private set; } = 0f;
+    protected Rect CurrentPosition => _currentPosition;
+    protected object CurrentControlResult => _currentControlResult;
     #endregion
 
     #region Methods
-    // Key Method ------------------------------------------------------------------------------------
-    protected virtual void ExecutePropertyLogic(Rect position, SerializedProperty property, GUIContent label)
+    // Key Methods ------------------------------------------------------------------------------------
+    protected abstract void ExecutePropertyLogic(SerializedProperty property, GUIContent label);
+    protected void ExecuteControlLogic(Action gui, float height = -1f)
     {
+        if (height <= 0f)
+            height = EditorGUIUtility.singleLineHeight;
 
+        if (EditorGUI)
+        {
+            _currentPosition.height = height;
+            gui.Invoke();
+            _currentPosition.y += height + EditorGUIUtility.standardVerticalSpacing;
+        }
+        else
+            PropertyHeight += height + EditorGUIUtility.standardVerticalSpacing;
     }
     // Overrides ---------------------------------------------------------------------------
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        _editorGUI = true;
-        ExecutePropertyLogic(position, property, label);
+        _currentPosition = position;
+        EditorGUI = true;
+        ExecutePropertyLogic(property, label);
     }
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        _editorGUI = false;
-        _propertyHeight = 0f;
-        ExecutePropertyLogic(Rect.zero, property, label);
-        return _propertyHeight;
+        _currentPosition = Rect.zero;
+        EditorGUI = false;
+        PropertyHeight = 0f;
+        ExecutePropertyLogic(property, label);
+        return PropertyHeight;
     }
+    // Delegate Methods -----------------------------------------------------------
+    protected Bounds BoundsField(Bounds value)
+    {
+        return EditorGUIControl(value, GetLineHeight(2), nameof(BoundsField), value);
+    }
+    protected Bounds BoundsField(GUIContent label, Bounds value)
+    {
+        return EditorGUIControl(value, GetLineHeight(2), nameof(BoundsField), label, value);
+    }
+    protected BoundsInt BoundsIntField(BoundsInt value)
+    {
+        return EditorGUIControl(value, GetLineHeight(2), nameof(BoundsIntField), value);
+    }
+    protected BoundsInt BoundsIntField(string label, BoundsInt value)
+    {
+        return EditorGUIControl(value, GetLineHeight(2), nameof(BoundsIntField), label, value);
+    }
+    protected BoundsInt BoundsIntField(GUIContent label, BoundsInt value)
+    {
+        return EditorGUIControl(value, GetLineHeight(2), nameof(BoundsIntField), label, value);
+    }
+    protected Color ColorField(Color value)
+    {
+        return EditorGUIControl(value, GetLineHeight(), nameof(ColorField), value);
+    }
+    protected Color ColorField(string label, Color value)
+    {
+        return EditorGUIControl(value, GetLineHeight(), nameof(ColorField), label, value);
+    }
+    protected Color ColorField(GUIContent label, Color value)
+    {
+        return EditorGUIControl(value, GetLineHeight(), nameof(ColorField), label, value);
+    }
+    protected Color ColorField(GUIContent label, Color value, bool showEyedropper, bool showAlpha, bool hdr)
+    {
+        return EditorGUIControl(value, GetLineHeight(), nameof(ColorField), label, value, showEyedropper, showAlpha, hdr);
+    }
+    //protected EditorGUIControl CurveField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DelayedDoubleField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DelayedFloatField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DelayedIntField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DelayedTextField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DoubleField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl DropdownButton()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl EnumFlagsField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl EnumPopup()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl FloatField()
+    //{
+    //    return Standard;
+    //}
+    protected bool Foldout(bool foldout, string content, GUIStyle style = null)
+    {
+        if (style == null)
+            style = EditorStyles.foldout;
+        return EditorGUIControl(foldout, GetLineHeight(), nameof(Foldout), foldout, content, style);
+    }
+    protected bool Foldout(bool foldout, string content, bool toggleOnLabelClick, GUIStyle style = null)
+    {
+        if (style == null)
+            style = EditorStyles.foldout;
+        return EditorGUIControl(foldout, GetLineHeight(), nameof(Foldout), foldout, content, toggleOnLabelClick, style);
+    }
+    protected bool Foldout(bool foldout, GUIContent content, GUIStyle style = null)
+    {
+        if (style == null)
+            style = EditorStyles.foldout;
+        return EditorGUIControl(foldout, GetLineHeight(), nameof(Foldout), foldout, content, style);
+    }
+    protected bool Foldout(bool foldout, GUIContent content, bool toggleOnLabelClick, GUIStyle style = null)
+    {
+        if (style == null)
+            style = EditorStyles.foldout;
+        return EditorGUIControl(foldout, GetLineHeight(), nameof(Foldout), foldout, content, toggleOnLabelClick, style);
+    }
+    //protected EditorGUIControl GradientField()
+    //{
+    //    return Standard;
+    //}
+    //// FLAG
+    //protected EditorGUIControl HelpBox()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl IntField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl IntPopup()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl IntSlider()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl LabelField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl LayerField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl LinkButton()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl LongField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl MaskField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl MinMaxSlider()
+    //{
+    //    return Standard;
+    //}
+    //// FLAG
+    //protected EditorGUIControl MultiFloatField()
+    //{
+    //    return Standard;
+    //}
+    //// FLAG
+    //protected EditorGUIControl MultiIntField()
+    //{
+    //    return Standard;
+    //}
+    //// FLAG
+    //protected EditorGUIControl MultiPropertyField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl ObjectField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl PasswordField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl Popup()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl PrefixLabel()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl ProgressBar()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl PropertyField()
+    //{
+    //    if (_property == null)
+    //        throw GUIException($"Cannot  the  for this editor GUI  " +
+    //            $"because no serialized property has been specified for it");
+    //    return EditorGUI.Property(_property, _label, _includeChildren);
+    //}
+    //protected EditorGUIControl RectField()
+    //{
+    //    return BoundsField();
+    //}
+    //protected EditorGUIControl RectIntField()
+    //{
+    //    return BoundsField();
+    //}
+    //protected EditorGUIControl SelectableLabel()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl Slider()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl TagField()
+    //{
+    //    return Standard;
+    //}
+    //// FLAG
+    //protected EditorGUIControl TextArea()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl TextField()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl Toggle()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl ToggleLeft()
+    //{
+    //    return Standard;
+    //}
+    //protected EditorGUIControl Vector2Field()
+    //{
+    //    if (EditorGUIUtility.wideMode)
+    //        return Standard;
+    //    else
+    //        return Standard * 2f;
+    //}
+    //protected EditorGUIControl Vector2Int()
+    //{
+    //    return Vector2Field();
+    //}
+    //protected EditorGUIControl Vector3Field()
+    //{
+    //    return Vector2Field();
+    //}
+    //protected EditorGUIControl Vector3IntField()
+    //{
+    //    return Vector2Field();
+    //}
+    //protected EditorGUIControl Vector4Field()
+    //{
+    //    return Vector2Field();
+    //}
     // Helper Methods ---------------------------------------------------------------
-    private TResult InvokeEditorGUIMethod<TResult>(string methodName, ref Rect position, TResult initialValue, params object[] otherArgs)
+    private float GetLineHeight(int lines = 1)
     {
-        float height = 0f;
-
-        if (_editorGUI)
-        {
-            // Setup the types
-            position.height = height;
-            Type type = typeof(EditorGUI);
-
-            // Setup the arguments
-            object[] args = new object[otherArgs.Length + 1];
-            args[0] = position;
-            for (int i = 1; i < args.Length; i++)
-                args[i] = otherArgs[i];
-
-            // Setup argument types
-            Type[] argTypes = new Type[args.Length];
-            for (int i = 0; i < args.Length; i++)
-            {
-                if (args[i] == null)
-                    throw GetGUIException($"Argument #{i + 1} cannot be null");
-                argTypes[i] = args[i].GetType();
-            }
-
-            // Try to get the method
-            MethodInfo method = type.GetMethod(methodName, argTypes);
-            object resultObject = method.Invoke(null, args);
-            try
-            {
-                return (TResult)resultObject;
-            }
-            catch (InvalidCastException)
-            {
-                throw GetGUIException(
-                    $"Method '{type}.{methodName}({GetTypesString(argTypes)})' returned an object " +
-                    $"of type '{resultObject.GetType()}' " +
-                    $"where an object of type '{typeof(TResult)}' was expected");
-            }
-        }
-        else
-        {
-            _propertyHeight += height;
-            return initialValue;
-        }
+        return (lines * EditorGUIUtility.singleLineHeight) + ((lines - 1) * EditorGUIUtility.standardVerticalSpacing);
     }
-    private MethodInfo GetEditorGUIMethod(string methodName, ref Rect position, params object[] otherArgs)
+    private TResult EditorGUIControl<TResult>(TResult value, float height, string methodName, params object[] args)
     {
-        // Setup the position height based on control height
-        position.height = 0f;
-
+        ExecuteControlLogic(GetEditorGUIMethodInvoker(methodName, args), height);
+        return GetInitialValueOrCurrentControlResult(value);
+    }
+    private Action GetEditorGUIMethodInvoker(string methodName, params object[] argsSansRect)
+    {
         // Setup the arguments
         Type type = typeof(EditorGUI);
-        object[] args = new object[otherArgs.Length + 1];
-        args[0] = position;
-        for (int i = 1; i < args.Length; i++)
-            args[i] = otherArgs[i];
-
-        // Setup argument types
-        Type[] argTypes = new Type[args.Length];
-        for (int i = 0; i < args.Length; i++)
+        Type[] argTypes = new Type[argsSansRect.Length + 1];
+        argTypes[0] = typeof(Rect);
+        for (int i = 0; i < argsSansRect.Length; i++)
         {
-            if (args[i] == null)
+            if (argsSansRect[i] == null)
                 throw GetGUIException($"Argument #{i + 1} cannot be null");
-            argTypes[i] = args[i].GetType();
+            argTypes[i + 1] = argsSansRect[i].GetType();
         }
 
         // Try to get the method
         MethodInfo method = type.GetMethod(methodName, argTypes);
         if (method == null)
             throw GetGUIException($"Method '{type}.{methodName}({GetTypesString(argTypes)})' does not exist");
-        position.y += position.height;
 
-        return method;
+        // We have to use a function that sets the position at invokation time
+        void InvokeMethod()
+        {
+            object[] args = new object[argsSansRect.Length + 1];
+            args[0] = _currentPosition;
+            argsSansRect.CopyTo(args, 1);
+            _currentControlResult = method.Invoke(null, args);
+        }
+
+        return InvokeMethod;
+    }
+    private TResult GetInitialValueOrCurrentControlResult<TResult>(TResult initialValue)
+    {
+        if (EditorGUI)
+            return (TResult)_currentControlResult;
+        else
+            return initialValue;
     }
     private string GetTypesString(Type[] types)
     {
